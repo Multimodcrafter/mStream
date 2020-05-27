9287e363523fc15c867a5b9b677731be6e6671e1
Robin Hänni <multimodcrafter@hotmail.com>
2020-05-27 16:00:02 +0200

    Revert "force unhandled error for electron version"
    This reverts commit 5f50a9f90a6833a760360da1648f0402f18ee996.

--- mstream.js
+++ mstream.js
@@ -15,7 +15,14 @@ const ddns = require('./modules/ddns');
 const federation = require('./modules/federation');
 
 exports.serveIt = config => {
-  const program = defaults.setup(config);
+  let program;
+  try {
+    program = defaults.setup(config);
+  } catch(err) {
+    winston.error('Config Validation Failed');
+    console.log(err);
+    process.exit(1);
+  }
 
   // Logging
   if (program.writeLogs) {
